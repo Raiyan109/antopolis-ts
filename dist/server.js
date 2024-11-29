e526a2d1374d40bf8bd1c0a89ba52077e15096bc
Raiyan109 <kraiyan109@gmail.com>
2024-11-29 11:01:38 +0600

    directly used db url from env in server.ts

--- dist/server.js
+++ dist/server.js
@@ -18,7 +18,7 @@ const config_1 = __importDefault(require("./app/config"));
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            yield mongoose_1.default.connect(config_1.default.database_url);
+            yield mongoose_1.default.connect(process.env.DATABASE_URL);
             app_1.default.listen(config_1.default.port, () => {
                 console.log(`app is listening on port ${config_1.default.port}`);
             });
