8fc57b697c69bc9647ccdefff25a093cb483b6cf
Raiyan109 <kraiyan109@gmail.com>
2024-11-29 11:59:11 +0600

    now deploy

--- dist/server.js
+++ dist/server.js
@@ -18,7 +18,7 @@ const config_1 = __importDefault(require("./app/config"));
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            yield mongoose_1.default.connect(process.env.DATABASE_URL);
+            yield mongoose_1.default.connect('mongodb+srv://antopolis:raTn5g1qAeU5b967@level-2.suciuav.mongodb.net/antopolis?retryWrites=true&w=majority&appName=Level-2');
             app_1.default.listen(config_1.default.port, () => {
                 console.log(`app is listening on port ${config_1.default.port}`);
             });
--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import config from './app/config';
 
 async function main() {
     try {
-        await mongoose.connect(process.env.DATABASE_URL as string);
+        await mongoose.connect('mongodb+srv://antopolis:raTn5g1qAeU5b967@level-2.suciuav.mongodb.net/antopolis?retryWrites=true&w=majority&appName=Level-2');
 
         app.listen(config.port, () => {
             console.log(`app is listening on port ${config.port}`);
